6d276d5f19b9bfd02729f6340a73143118193b6b
Mohit Verma <100288392+BrilliantCoderfirst@users.noreply.github.com>
2022-06-30 10:40:34 +0530

    Add files via upload

---
+++ potfolio/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // It's not working
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    var typed = new Typed(".typing", {
+        strings: ["Hardware", "Coder", "Programmer", "Designer", "Freelancer"],
+        typedSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Hardware", "Coder", "Programmer", "Designer", "Freelancer"],
+        typedSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    $('.menu-btu').click(function(){
+        $('.navbar .nav-2').toggleClass("active");
+        $('.menu-btu i').toggleClass("active");
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+});
